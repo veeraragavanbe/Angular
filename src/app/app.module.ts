e574af5622d0f3679c65ce6cc465911caca5e5d9
veeraragavanB <veeraragavan.b@techath.com>
2021-05-14 22:22:22 +0530

    hiding data

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { EventsAppComponent } from './events-app.component';
 import { EventsListcomponent} from './events/events-list.component';
 import { EventThumbnailComponent} from './events/events-thumbnails.component';
-
+import { NavBarComponent } from './nav/navbar.component'
 @NgModule({
   declarations: [
     EventsAppComponent,
     EventsListcomponent,
-    EventThumbnailComponent
+    EventThumbnailComponent,
+    NavBarComponent
   ],
   imports: [
     BrowserModule
--- src/app/events/events-thumbnails.component.ts
+++ src/app/events/events-thumbnails.component.ts
@@ -1,30 +1,34 @@
-
 import { summaryForJitFileName } from "@angular/compiler/src/aot/util";
-import { Component, Input , Output, EventEmitter} from "@angular/core";
+import { Component, Input} from "@angular/core";
 
 @Component ({
     selector: 'event-thumbnail',    
-    template: `  <div class="well">
-                 <h2>{{event.name}}</h2>
-                 <div>Id: {{event.id}}</div>
-                 <div>Date: {{event.date}}</div>
-                 <div>Time: {{event.time}}</div>
-                 <div>Price: \${{event.price}}</div>
-                 <div>Location: <span>{{event.location.address}}</span>
-                 <span>&nbsp;</span>
-                 <span>{{event.location.city}},{{event.location.pin}}</span></div>
+    template: `  <div class="well thumbnail">
+                 <h2>{{event?.name}}</h2>
+                 <div>Id: {{event?.id}}</div>
+                 <div>Date: {{event?.date}}</div>
+                 <div>Time: {{event?.time}}</div>
+                 <div>Price: \${{event?.price}}</div>
+                 <div [hidden] = "!event?.location"  >
+                      <span>Location:{{event?.location?.address}}</span>
+                 <span class="pad-lef">{{event?.location?.city}},{{event?.location?.pin}}</span>
+                </div>
+                 <div [hidden] = "!event?.OnlineUrl">
+                     Online Url: {{ event.OnlineUrl}}
+                </div>
                  
                  </div>
-                 `
+                 `,
+                 styles:[`
+                 .pad-lef{margin-left: 5px;}
+                 .well div{color: #bbb;}
+                 .thumbnail{min-height: 230px}
+                 `]
 })
 export class EventThumbnailComponent
 {
     @Input() event:any 
 
-    logfoo()
-    {
-        console.log('foo this is veer')
-    }
 
 }
 
@@ -38,3 +42,20 @@ export class EventThumbnailComponent
     // {
     //     this.eventClick.emit(this.event.name) 
     // }
+
+    //thumbnail variable (template var)
+
+    // logfoo()
+    // {
+    //     console.log('foo this is veer')
+    // }
+
+    //2
+    // someProperty:any = "thumbnail 2"
+
+
+    // used to hide the value if it not present in data arrary or object
+    // *ngIf="event?.location"   ==== if the location not present in the array it just hide that element
+    //  ? before event helps to run code without error. if the data not binded or present in array
+    // another way to hidding
+    // <div [hidden] = "!event?.location">
