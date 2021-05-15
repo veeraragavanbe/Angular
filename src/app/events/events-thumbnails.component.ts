2bd628cd4849b711ef6900436ee41f1169b0d035
veeraragavanB <veeraragavan.b@techath.com>
2021-05-15 09:47:56 +0530

    ngclas

--- src/app/events/events-thumbnails.component.ts
+++ src/app/events/events-thumbnails.component.ts
@@ -7,7 +7,12 @@ import { Component, Input} from "@angular/core";
                  <h2>{{event?.name}}</h2>
                  <div>Id: {{event?.id}}</div>
                  <div>Date: {{event?.date}}</div>
-                 <div>Time: {{event?.time}}</div>
+                 <div [class.green]="event?.time === '8.00 am'" [ngSwitch] = "event?.time">
+                    Time: {{event?.time}}
+                    <span *ngSwitchCase="'8:00 am'">(Early start)</span>
+                    <span *ngSwitchCase="'10:00 am'">(Late start)</span>
+                    <span *ngSwitchDefault>(Normal Start)</span>
+                 </div>
                  <div>Price: \${{event?.price}}</div>
                  <div [hidden] = "!event?.location"  >
                       <span>Location:{{event?.location?.address}}</span>
@@ -21,14 +26,24 @@ import { Component, Input} from "@angular/core";
                  `,
                  styles:[`
                  .pad-lef{margin-left: 5px;}
+                 .green {color: #003300;}
                  .well div{color: #bbb;}
-                 .thumbnail{min-height: 230px}
+                 .thumbnail{min-height: 230px;}
+                 
+                 .bold {font-weight: bold;}
                  `]
 })
 export class EventThumbnailComponent
 {
     @Input() event:any 
 
+    // getStartClass()
+    // {
+    //     if(this.event && this.event.time === '8.00 am')
+    //         return 'green bold'
+    //     return '' 
+        
+    // }
 
 }
 
