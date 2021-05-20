22634b46e6fe7e2497afa8e886a95e67d86f4603
veeraragavanB <veeraragavan.b@techath.com>
2021-05-20 22:22:20 +0530

    user login

--- src/app/user/login.component.ts
+++ src/app/user/login.component.ts
@@ -4,11 +4,15 @@ import { Router } from "@angular/router";
 
 @Component({
     templateUrl: './login.component.html',
+    styles: [`
+                em {float:right;color:#E05C65;padding-left:10px}
+            `],
 })
 
 export class LoginComponent{
     userName
     password
+    mouseoverLogin
 
     constructor(private authService:AuthService, private router:Router)
     {}
--- src/app/user/user.module.ts
+++ src/app/user/user.module.ts
@@ -1,17 +1,19 @@
 import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
 import { RouterModule } from "@angular/router";
+import { FormsModule } from "@angular/forms";
+import { ReactiveFormsModule } from "@angular/forms";
 import { userRoutes } from "./user.routes";
 import { ProfileComponent } from "./profile.component";
 import { LoginComponent } from "./login.component";
-import { FormsModule } from "@angular/forms";
 
 
 @NgModule({
     imports:[
         CommonModule,
         FormsModule,
-        RouterModule.forChild( userRoutes)
+        RouterModule.forChild( userRoutes),
+        ReactiveFormsModule
     ],
     declarations:[
         ProfileComponent,
